13577528b6550c3f244a8890f3e77f782bb052e0
MourierGr <59049859+MourierGr@users.noreply.github.com>
2019-12-19 11:02:39 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,19 @@
-packageTP2;importjava.util.Scanner;classex5{publicstaticvoidmain(String[] args){intnbre= -1;intres;inti;Scanner scan= newScanner(System.in);System.out.println("Entrer le chiffre à multiplier:");nbre=scan.nextInt();for(i=1; i<11; i++){res= i*nbre;System.out.print(i);System.out.print("x"+ nbre+ "=");System.out.println(res);}scan.close();}}
+packageTP2;
+importjava.util.Scanner;
+classe test{
+  public static void main(String[] args){
+    int nbre= -1;
+    int res;
+    int i;
+    Scanner scan= new Scanner(System.in);
+    System.out.println("Entrer le chiffre à multiplier:");
+    nbre=scan.nextInt();
+    for(i=1; i<11; i++){
+      res= i*nbre;
+      System.out.print(i);
+      System.out.print("x"+ nbre+ "=");
+      System.out.println(res);
+    }
+    scan.close();
+  }
+}
